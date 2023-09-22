963af0997649c11bc4aedf2011b7f2eb0865cdcd
kardelendemir <145125034+kardelendemir@users.noreply.github.com>
2023-09-22 17:10:34 +0300

    Add files via upload

---
+++ uygulama6/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace uygulama6
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            { .text = "1"; }
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+            { label1.Text = "6"; }
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+            { label1.Text = "9"; }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            { label1.Text = "1"; }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            { label1.Text = "2"; }
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            { label1.Text = "3"; }
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            { label1.Text = "4"; }
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+            { label1.Text = "5"; }
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+            { label1.Text = "7"; }
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+            { label1.Text = "8"; }
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+            { label1.Text = "0"; }
+        }
+    }
+}
